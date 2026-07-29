99e00dc38e8298d609a6b2eab5c3bc5cdb746699
aquamancer <aquamancer90@gmail.com>
2026-07-28 23:33:42 -0700

    try to fix desync bug

--- src/main/java/com/aquamancer/czlib/mixin/ClientConnectionMixin.java
+++ src/main/java/com/aquamancer/czlib/mixin/ClientConnectionMixin.java
@@ -3,6 +3,7 @@ package com.aquamancer.czlib.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.ClickSlotC2SPacket;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,6 +16,9 @@ public class ClientConnectionMixin {
     @Inject(at = @At("HEAD"), method = "send(Lnet/minecraft/network/packet/Packet;)V")
     private void onSend(Packet<?> packet, CallbackInfo ci) {
         MinecraftClient.getInstance().execute(() -> {
+            if (packet instanceof ClickSlotC2SPacket p) {
+                MinecraftClient.getInstance().player.sendMessage(Text.literal("click slot syncid: " + p.getSyncId()));
+            }
 //            if (packet instanceof CloseHandledScreenC2SPacket c) {
 //                MinecraftClient.getInstance().player.sendMessage(Text.literal("close screen c2s: " + c.getSyncId()));
 //            }
--- src/main/java/com/aquamancer/czlib/mixin/ScreenCancelerMixin.java
+++ src/main/java/com/aquamancer/czlib/mixin/ScreenCancelerMixin.java
@@ -4,6 +4,8 @@ import com.aquamancer.czlib.internal.ScreenCanceler;
 import com.aquamancer.czlib.internal.UpdateManager;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
@@ -18,9 +20,10 @@ public class ScreenCancelerMixin {
     private void setScreen(@Nullable Screen screen, CallbackInfo ci) {
         MinecraftClient client = MinecraftClient.getInstance();
         if (screen == null || client == null || client.player == null) return;
-//        client.player.sendMessage(Text.literal(screen.getClass().getCanonicalName()).append(Text.literal(": ")).append(screen.getTitle()));
+        if (client.currentScreen instanceof HandledScreen<?>) return;  // for some reason canceling while a screen is open disables it
         if (ScreenCanceler.shouldCancelScreen(screen)) {
             UpdateManager.sendPacket(new CloseHandledScreenC2SPacket(client.player.currentScreenHandler.syncId));
+            client.player.sendMessage(Text.literal("canceled screen syncid="+client.player.currentScreenHandler.syncId));
             ci.cancel();
         }
     }
