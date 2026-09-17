c39e7244d3db1122192bda328170fe23e8a94a69
aquamancer <aquamancer90@gmail.com>
2026-09-17 11:50:37 -0400

    prevent full updates inside utility rooms

--- src/main/java/com/aquamancer/czlib/internal/UpdateManager.java
+++ src/main/java/com/aquamancer/czlib/internal/UpdateManager.java
@@ -186,6 +186,7 @@ public class UpdateManager {
         if (client.currentScreen instanceof HandledScreen) return;
         if (ScreenCanceler.isCancelingScreens()) return;
         if (ZenithApi.getInstance().getCurrentRoomType() == Room.TREE_SELECT) return;
+        if (ZenithApi.getInstance().getCurrentRoomType() == Room.UTILITY) return;
         if (ZenithApi.getInstance().roomRewardFound()) return;
         if (ticksSinceFullUpdate < MIN_TICKS_BETWEEN_FULL_UPDATE) {
             ticksUntilUpdate = MIN_TICKS_BETWEEN_FULL_UPDATE - ticksSinceFullUpdate;
